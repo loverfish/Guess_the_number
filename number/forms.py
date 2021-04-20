fe3de448e13db92c66ada187a0ab6b33483a284d
loverfish <blackmemberb@gmail.com>
2021-04-20 18:10:32 +0300

    Fixed input bugs

--- number/forms.py
+++ number/forms.py
@@ -1,10 +1,10 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Number
 
 
 class NumberForm(forms.ModelForm):
-    # digits_quantity = form.IntegerField(min_value=30)
     class Meta:
         model = Number
         fields = ['digits_quantity']
@@ -18,6 +18,12 @@ class NumberForm(forms.ModelForm):
             }
         )}
 
+    def clean_digits_quantity(self):
+        new_digits_quantity = self.cleaned_data['digits_quantity']
+        if new_digits_quantity not in range(1, 19):
+            raise ValidationError('Введите количество цифр в диапозоне от 1 до 18')
+        return new_digits_quantity
+
 
 class InputForm(forms.Form):
     number = forms.IntegerField(
@@ -27,3 +33,10 @@ class InputForm(forms.Form):
             'placeholder': 'Введите число',
         })
     )
+
+    def clean_number(self):
+        len_start_number = len(str(Number.objects.last().numb))
+        new_number = str(self.cleaned_data['number'])
+        if len(new_number) != len_start_number:
+            raise ValidationError('Введите число из {} цифр'.format(len_start_number))
+        return new_number
--- number/views.py
+++ number/views.py
@@ -8,9 +8,12 @@ from .utils import create_number, compare_number
 def start_game(request):
     form = NumberForm
     if request.method == 'POST':
-        digits_quantity = int(request.POST['digits_quantity'])
-        Number.objects.create(numb=create_number(digits_quantity))
-        return redirect('guess')
+        bound_form = NumberForm(request.POST)
+        if bound_form.is_valid():
+            digits_quantity = int(request.POST['digits_quantity'])
+            Number.objects.create(numb=create_number(digits_quantity))
+            return redirect('guess')
+        form = bound_form
     return render(request, 'number/start_game.html', {'form': form})
 
 
@@ -21,15 +24,19 @@ def guess(request):
         'len': len(start_number),
     }
     if request.method == 'POST':
-        input_number = request.POST['number']
-        right_place, correct_digit = compare_number(input_number, start_number)
-        if right_place == context['len']:
-            return render(request, 'number/finish.html')
-        context.update({
-            'response': True,
-            'right_place': right_place,
-            'correct_digit': correct_digit,
-        })
+        bound_form = InputForm(request.POST)
+        if bound_form.is_valid():
+            input_number = request.POST['number']
+            right_place, correct_digit = compare_number(input_number, start_number)
+            if right_place == context['len']:
+                return render(request, 'number/finish.html')
+            context.update({
+                'response': True,
+                'right_place': right_place,
+                'correct_digit': correct_digit,
+            })
+        context['form'] = bound_form
+
     return render(request, 'number/guess.html', {'context': context})
 
 
